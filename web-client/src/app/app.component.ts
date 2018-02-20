ec67920d30af0f31a9e91f626bd3486a38c38afb
Alessandro Cappellozza <a.cappellozza@seneca.it>
2018-02-20 14:59:25 +0100

    - Release 1.0.1.0 - New panel to read the device info like hardware and firmware revision.

--- web-client/src/app/app.component.ts
+++ web-client/src/app/app.component.ts
@@ -1,32 +1,45 @@
-import { Component } from '@angular/core';
-import { Mqtt } from './services/mqtt';
+import {Component} from '@angular/core';
+import {Mqtt} from './services/mqtt';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  events = [];
-  event_notification = false;
-  status = null;
+    title = 'app';
+    dev = null;
+    events = [];
+    event_notification = false;
+    status = null;
 
-  constructor(public mqtt: Mqtt) {
-    this.status = this.mqtt.subscribe('sts');
+    constructor(public mqtt: Mqtt) {
+        this.status = this.mqtt.subscribe('sts');
 
-    this.mqtt.subscribe('ev').subscribe((msg) => {
-      if (!msg.retain) {
-        this.events.push(msg);
-        this.event_notification = true;
-        setTimeout(() => this.event_notification = false, 2500);
-      } else {
-        console.log('RETAIN MESSAGE IGNORED', msg);
-      }
-    });
-  }
+        this.mqtt.subscribe('cfg').subscribe((msg) => {
+            let cfg = JSON.parse(msg.payload.toString());
+            console.log("CONFIGURATION RECEIVED", cfg);
+            if (cfg['dev']) {
+                this.dev = cfg.dev;
+            }
+        });
 
-  ResetEvents() {
-    this.events = [];
-  }
+        this.mqtt.subscribe('ev').subscribe((msg) => {
+            if (!msg.retain) {
+                this.events.push(msg);
+                this.event_notification = true;
+                setTimeout(() => this.event_notification = false, 2500);
+            } else {
+                console.log('RETAIN MESSAGE IGNORED', msg);
+            }
+        });
+    }
+
+    ResetEvents() {
+        this.events = [];
+    }
+
+    ReadDeviceInfo() {
+        this.mqtt.publish('act', { act: 186712324 });
+    }
 }
